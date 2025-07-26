7396f4336aa2d1d4f3fd59fef29150ac1781c296
ARIKBOGA <barikboga42@gmail.com>
2025-07-27 00:05:58 +0300

    Refactor TestComponent structure and improve test cases for clarity

--- src/App.spec.js
+++ src/App.spec.js
@@ -2,16 +2,22 @@ import { render, screen } from "@testing-library/react";
 
 function TestComponent() {
   return (
-    <form>
-      <label htmlFor="user-name">User name:</label>
-      <input type="text" id="user-name" placeholder="Enter your username" />
-      <input
-        placeholder="Enter your email"
-        defaultValue={"test@mail.com"}
-      ></input>
-      <button type="submit">Log in</button>
-      <img src='#' alt='TestComponent iamge'></img>
-    </form>
+    <>
+      <form>
+        <label htmlFor="user-name">User name:</label>
+        <input type="text" id="user-name" placeholder="Enter your username" />
+        <input
+          placeholder="Enter your email"
+          defaultValue={"test@mail.com"}
+        ></input>
+        <button type="submit">Log in</button>
+        <img src="#" alt="TestComponent iamge"></img>
+      </form>
+      <div data-testid="divElement">MODERN TESTING WITH JEST</div>
+      <div>
+        <button role="navigation">Home</button>
+      </div>
+    </>
   );
 }
 
@@ -37,14 +43,27 @@ it("should render with it's text content", () => {
   expect(element).toHaveTextContent("Log in");
 });
 
-it('should render with its displayed value', () => {
-    render(<TestComponent />);
-    const element = screen.getByDisplayValue("test@mail.com");
-    expect(element).toBeInTheDocument();
-})
+it("should render with its displayed value", () => {
+  render(<TestComponent />);
+  const element = screen.getByDisplayValue("test@mail.com");
+  expect(element).toBeInTheDocument();
+});
 
 it("should render with it's alt text for images", () => {
-    render(<TestComponent />);
-    const element = screen.getByAltText("TestComponent iamge");
-    expect(element.src).toBe('http://localhost/#');     // despite the src value has been given as '#', it will processed as 'http://localhost/#'
-})
+  render(<TestComponent />);
+  const element = screen.getByAltText("TestComponent iamge");
+  expect(element.src).toBe("http://localhost/#"); // despite the src value has been given as '#', it will processed as 'http://localhost/#'
+});
+
+it("should render with it's test id", () => {
+  render(<TestComponent />);
+  const element = screen.getByTestId("divElement");
+  expect(element).toBeInTheDocument();
+  expect(element.textContent).toBe("MODERN TESTING WITH JEST");
+});
+
+it("should render with it's role", () => {
+  render(<TestComponent />);
+  const element = screen.getByRole("navigation");
+  expect(element.textContent).toBe("Home");
+});
